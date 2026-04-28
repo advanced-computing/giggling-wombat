a6193b67be37b18a8bc7e8f7b1871554818cf77a
indrasuhyar <imsuhyar@gmail.com>
2026-04-27 22:06:50 -0400

    fix: remove unused go import, apply ruff format

--- Homepage.py
+++ Homepage.py
@@ -2,7 +2,6 @@ import time
 
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
 from google.cloud import bigquery
 from google.oauth2 import service_account
